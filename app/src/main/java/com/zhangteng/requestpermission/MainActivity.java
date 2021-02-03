8544985a55eeae23c9f1052b1f27ca4dac141382
Swing <763263311@qq.com>
2021-02-03 15:28:43 +0800

    增加回调参数PermissionActivity&透明背景

--- app/src/main/java/com/zhangteng/requestpermission/MainActivity.java
+++ app/src/main/java/com/zhangteng/requestpermission/MainActivity.java
@@ -1,8 +1,10 @@
 package com.zhangteng.requestpermission;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Toast;
 
 import com.zhangteng.androidpermission.AndroidPermission;
@@ -17,30 +19,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        androidPermission = new AndroidPermission.Buidler()
-                .with(this)
-                .permission(Permission.Group.CALENDAR)
-                .callback(new Callback() {
-                    @Override
-                    public void success() {
-                        Toast.makeText(MainActivity.this, "success", Toast.LENGTH_SHORT).show();
-                    }
-
-                    @Override
-                    public void failure() {
-                        Toast.makeText(MainActivity.this, "failure", Toast.LENGTH_SHORT).show();
-                    }
-
-                    @Override
-                    public void nonExecution() {
-                        Toast.makeText(MainActivity.this, "nonExecution", Toast.LENGTH_SHORT).show();
-                    }
-                })
-                .build();
-        //用于初次请求权限
-        androidPermission.excute();
-        //用于再次请求权限
-        androidPermission.excute(100);
     }
 
     @Override
@@ -49,5 +27,37 @@ public class MainActivity extends AppCompatActivity {
         if (requestCode == 100 && androidPermission.checkPermission()) {
             Toast.makeText(MainActivity.this, "从设置页返回", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    public void onClick(View view) {
+        String[] permissions = new String[]{
+                Permission.ACCESS_FINE_LOCATION,
+                Permission.ACCESS_COARSE_LOCATION,
+                Permission.READ_CALENDAR,
+                Permission.WRITE_CALENDAR};
+        androidPermission = new AndroidPermission.Buidler()
+                .with(this)
+                .permission(permissions)
+                .callback(new Callback() {
+                    @Override
+                    public void success(Activity permissionActivity) {
+                        Toast.makeText(MainActivity.this, "success", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void failure(Activity permissionActivity) {
+                        Toast.makeText(MainActivity.this, "failure", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void nonExecution(Activity permissionActivity) {
+                        Toast.makeText(MainActivity.this, "nonExecution", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .build();
+        //用于初次请求权限
+        androidPermission.execute();
+        //用于再次请求权限
+//        androidPermission.execute(100);
     }
 }
